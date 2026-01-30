dbac3c1debeed0df250009658da2e9aeb1f9daf4
Miras <meirambekmiras71@gmail.com>
2026-01-30 20:14:33 +0500

    Add Practice1 Python basics examples

---
+++ Practice1/python-basics/casting4.py
@@ -0,0 +1,4 @@
+x = bool(1)        # x will be True
+y = bool(0)        # y will be False
+z = bool("")       # z will be False
+w = bool("text")   # w will be True
---
+++ Practice1/python-basics/casting5.py
@@ -0,0 +1,5 @@
+x = list("abc")        # x will be ['a', 'b', 'c']
+y = list((1, 2, 3))    # y will be [1, 2, 3]
+
+z = tuple([4, 5, 6])   # z will be (4, 5, 6)
+w = tuple("hi")        # w will be ('h', 'i')
